e36639b72d192ef8bceea07e39f32a504c885d97
Bas Zalmstra <zalmstra.bas@gmail.com>
2022-03-31 23:17:46 +0200

    misc: replace prefix

--- crates/rattler/src/install.rs
+++ crates/rattler/src/install.rs
@@ -1,6 +1,5 @@
 use crate::package_archive::{FileMode, Index, PathEntry, PathType, Paths};
 use anyhow::Context;
-use bytes::Bytes;
 use futures::stream::FuturesUnordered;
 use futures::{FutureExt, TryFutureExt, TryStreamExt};
 use itertools::Itertools;
@@ -9,6 +8,7 @@ use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 use reqwest_retry::policies::ExponentialBackoff;
 use reqwest_retry::RetryTransientMiddleware;
 use sha2::{Digest, Sha256};
+use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use thiserror::Error;
@@ -129,6 +129,8 @@ pub async fn install_package(
         let _ = link_task?;
     }
 
+    log::info!("finished linking {}", &package_file_name);
+
     Ok(())
 }
 
@@ -175,7 +177,7 @@ fn link_entry(
 
     // If the path already exists, remove it
     // TODO: Properly handle clobbering here
-    if destination_path.exists() {
+    if destination_path.is_file() {
         log::warn!(
             "Clobbering: $CONDA_PREFIX/{}",
             entry.relative_path.display()
@@ -184,77 +186,188 @@ fn link_entry(
             .with_context(|| format!("error removing existing file"))?;
     }
 
-    if let Some(prefix) = &entry.prefix_placeholder {
-        log::warn!(
-            "not implemented: replacing prefix in `{}`",
-            entry.relative_path.display()
-        );
-    } else {
-        // Determine how to copy the file
-        let mut link_type = if entry.path_type == PathType::HardLink && !entry.no_link {
-            LinkType::HardLink
-        } else if entry.path_type == PathType::SoftLink && !entry.no_link {
-            LinkType::SoftLink
-        } else {
-            LinkType::Copy
+    if let Some(old_prefix) = &entry.prefix_placeholder {
+        // Determine the new prefix for the file
+        let new_prefix = &prefix.to_string_lossy();
+        let digest = match entry.file_mode {
+            FileMode::Text => {
+                // TODO: Replace '\\' with '/' in prefix on windows
+                copy_replace_prefix_text(&source_path, &destination_path, old_prefix, &new_prefix)?
+            }
+            FileMode::Binary => {
+                let source_meta = std::fs::metadata(&source_path)
+                    .context("unable to determine permissions of cached file")?;
+                let digest = copy_replace_prefix_binary(
+                    &source_path,
+                    &destination_path,
+                    old_prefix,
+                    &new_prefix,
+                )?;
+                std::fs::set_permissions(destination_path, source_meta.permissions())
+                    .context("unable to assign same permissions as source file")?;
+                digest
+            }
         };
 
-        loop {
-            match link_type {
-                LinkType::HardLink => match std::fs::hard_link(&source_path, &destination_path) {
-                    Err(_) => {
-                        log::trace!(
-                            "error hard linking `{}` --> `{}`, trying soft linking",
-                            source_path.display(),
-                            destination_path.display()
-                        );
-                        link_type = LinkType::SoftLink;
-                    }
-                    Ok(_) => {
-                        log::trace!(
-                            "hard linked `{}` --> `{}`",
-                            source_path.display(),
-                            destination_path.display()
-                        );
-                        break;
-                    }
-                },
-                LinkType::SoftLink => match std::fs::soft_link(&source_path, &destination_path) {
-                    Err(_) => {
-                        log::trace!(
-                            "error soft linking `{}` --> `{}`, trying copying",
-                            source_path.display(),
-                            destination_path.display()
-                        );
-                        link_type = LinkType::Copy;
-                    }
-                    Ok(_) => {
-                        log::trace!(
-                            "soft linked `{}` --> `{}`",
-                            source_path.display(),
-                            destination_path.display()
-                        );
-                        break;
-                    }
-                },
-                LinkType::Copy => {
-                    match std::fs::copy(&source_path, &destination_path) {
-                        Err(err) => return Err(err.into()),
-                        Ok(_) => {
-                            log::trace!(
-                                "copied `{}` --> `{}`",
-                                source_path.display(),
-                                destination_path.display()
-                            );
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
+        return Ok((digest, entry.relative_path.clone()));
+    } else if entry.path_type == PathType::HardLink && !entry.no_link {
+        hard_link_entry(&source_path, &destination_path)?;
+    } else if entry.path_type == PathType::SoftLink && !entry.no_link {
+        soft_link_entry(&source_path, &destination_path)?;
+    } else {
+        copy_entry(&source_path, &destination_path)?;
+    };
 
     Ok((entry.sha256.clone(), entry.relative_path.clone()))
+}
+
+/// Copy the file from the source to the destination while replacing the `old_prefix` with the
+/// `new_prefix` in binary occurrences.
+fn copy_replace_prefix_binary(
+    source_path: &Path,
+    destination_path: &Path,
+    old_prefix: &str,
+    new_prefix: &str,
+) -> anyhow::Result<String> {
+    // Memory map the source file
+    let source = {
+        let file = std::fs::File::open(source_path).context("unable to open file from cache")?;
+        unsafe { memmap2::Mmap::map(&file) }.context("unable to memory map file from cache")?
+    };
+
+    // Open the output file for writing
+    let mut destination = std::fs::File::create(destination_path)
+        .context("unable to open destination file for writing")?;
+
+    // Get the prefixes as bytes
+    let old_prefix = old_prefix.as_bytes();
+    let new_prefix = new_prefix.as_bytes();
+
+    let padding_len = if old_prefix.len() > new_prefix.len() {
+        old_prefix.len() - new_prefix.len()
+    } else {
+        0
+    };
+    let padding = vec![0u8; padding_len];
+
+    let mut digest = Sha256::new();
+    let mut source_bytes = source.as_ref();
+    loop {
+        if let Some(index) = twoway::find_bytes(source_bytes, old_prefix) {
+            // Find the end of the c-style string
+            let mut end = index + old_prefix.len();
+            while end < source.len() && source_bytes[end] != 0 {
+                end += 1;
+            }
+
+            // Get the suffix part
+            let suffix = &source_bytes[index + old_prefix.len()..end];
+
+            // Write to disk
+            destination
+                .write_all(&source_bytes[..index])
+                .and_then(|_| destination.write_all(new_prefix))
+                .and_then(|_| destination.write_all(suffix))
+                .and_then(|_| destination.write_all(&padding))
+                .context("failed to write to destination")?;
+
+            // Update digest
+            digest.update(&source_bytes[..index]);
+            digest.update(new_prefix);
+            digest.update(suffix);
+            digest.update(&padding);
+
+            // Continue with the rest of the bytes
+            source_bytes = &source_bytes[end..];
+        } else {
+            // Write to disk
+            destination
+                .write_all(&source_bytes)
+                .context("failed to write to destination")?;
+
+            // Update digest
+            digest.update(&source_bytes);
+
+            return Ok(format!("{:x}", digest.finalize()));
+        }
+    }
+}
+
+/// Copy the file from the source to the destination while replacing the `old_prefix` with the
+/// `new_prefix` by searching for text occurrences.
+fn copy_replace_prefix_text(
+    source_path: &Path,
+    destination_path: &Path,
+    old_prefix: &str,
+    new_prefix: &str,
+) -> anyhow::Result<String> {
+    // Memory map the source file
+    let source = {
+        let file = std::fs::File::open(source_path).context("unable to open file from cache")?;
+        unsafe { memmap2::Mmap::map(&file) }.context("unable to memory map file from cache")?
+    };
+
+    // Open the output file for writing
+    let mut destination = std::fs::File::create(destination_path)
+        .context("unable to open destination file for writing")?;
+
+    // Get the prefixes as bytes
+    let old_prefix = old_prefix.as_bytes();
+    let new_prefix = new_prefix.as_bytes();
+
+    // TODO: Update shebang if present
+
+    let mut digest = Sha256::new();
+    let mut source_bytes = source.as_ref();
+    loop {
+        if let Some(index) = twoway::find_bytes(source_bytes, old_prefix) {
+            // Write to disk
+            destination
+                .write_all(&source_bytes[..index])
+                .and_then(|_| destination.write_all(new_prefix))
+                .context("failed to write to destination")?;
+
+            // Update digest
+            digest.update(&source_bytes[..index]);
+            digest.update(new_prefix);
+
+            source_bytes = &source_bytes[index + old_prefix.len()..];
+        } else {
+            // Write to disk
+            destination
+                .write_all(&source_bytes)
+                .context("failed to write to destination")?;
+
+            // Update digest
+            digest.update(&source_bytes);
+
+            return Ok(format!("{:x}", digest.finalize()));
+        }
+    }
+}
+
+/// Hard links an entry from the source archive to the destination. Falls back to soft-linking or
+/// copying if hard-linking fails.
+fn hard_link_entry(source_path: &Path, destination_path: &Path) -> anyhow::Result<()> {
+    std::fs::hard_link(source_path, destination_path)
+        .or_else(|_| std::fs::soft_link(source_path, destination_path))
+        .or_else(|_| std::fs::copy(source_path, destination_path).map(|_| ()))
+        .context("error hard linking entry")
+}
+
+/// Soft links an entry from the source archive to the destination. Falls back to copying if
+/// soft-linking fails.
+fn soft_link_entry(source_path: &Path, destination_path: &Path) -> anyhow::Result<()> {
+    std::fs::soft_link(source_path, destination_path)
+        .or_else(|_| std::fs::copy(source_path, destination_path).map(|_| ()))
+        .context("error soft linking entry")
+}
+
+/// Copies an entry from the source archive to the destination.
+fn copy_entry(source_path: &Path, destination_path: &Path) -> anyhow::Result<()> {
+    std::fs::copy(source_path, destination_path)
+        .map(|_| ())
+        .context("error copying entry")
 }
 
 enum LinkType {
@@ -445,14 +558,22 @@ async fn validate_package_entry(
 ) -> Result<(), ValidationError> {
     let entry_path = archive_path.join(&entry.relative_path);
 
-    let metadata = tokio::fs::metadata(&entry_path).await.map_err(|e| {
-        ValidationError::FileMetaDataError(entry.relative_path.display().to_string(), e)
-    })?;
-
-    if metadata.is_symlink() {
-        // TODO: Do something with this
-        return Ok(());
-    }
+    let metadata = match tokio::join!(
+        tokio::fs::metadata(&entry_path),
+        tokio::fs::symlink_metadata(&entry_path)
+    ) {
+        (Err(e), Err(_)) => {
+            return Err(ValidationError::FileMetaDataError(
+                entry.relative_path.display().to_string(),
+                e,
+            ))
+        }
+        (_, Ok(_)) => {
+            // TODO: Do something with this?
+            return Ok(());
+        }
+        (Ok(metadata), _) => metadata,
+    };
 
     // Make sure the file is a file, and not something else
     if !metadata.is_file() {
